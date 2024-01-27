87028dcac3411b6f6e23a6e8f044fbce52b6f4e3
AhsanSium <ahsansium28@gmail.com>
2024-01-27 17:53:37 +0600

    Module 3 Edge List

--- Intro to Algorithms and Problem Solving/Module 3/Edge List.cpp
+++ Intro to Algorithms and Problem Solving/Module 3/Edge List.cpp
@@ -6,15 +6,16 @@ int main()
     int nodes = 4;
     vector< vector <int> >edge_list;
 
-    edge_list.push_back({0, 1});
-    edge_list.push_back({1, 2});
-    edge_list.push_back({1, 3});
-    edge_list.push_back({2, 3});
+    //               edge1 edge2 weight
+    edge_list.push_back({0, 1, 2});
+    edge_list.push_back({1, 2, 4});
+    edge_list.push_back({1, 3, 9});
+    edge_list.push_back({2, 3, 1});
 
     // time O(E), space O(E)
     for(int i = 0; i < edge_list.size(); i++)
     {
-        cout << edge_list[i][0] << " " << edge_list[i][1] << endl;
+        cout << edge_list[i][0] << " " << edge_list[i][1] << " " <<edge_list[i][2] << endl;
 
     }
 
